ff750307ff6ced0eca358813822b353f3975e104
baltamas <70895239+baltamas@users.noreply.github.com>
2021-05-18 12:49:47 +0300

    -added movies service

--- MultimediaCenter/ClientApp/src/app/movies/movies-list/movies-list.component.ts
+++ MultimediaCenter/ClientApp/src/app/movies/movies-list/movies-list.component.ts
@@ -1,24 +1,27 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Movie } from '../movie.model';
+import { MoviesService } from '../movies.service';
 
 @Component({
   selector: 'app-list-movies',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css']
 })
-export class MoviesListComponent implements OnInit {
+export class MoviesListComponent{
 
   public movies: Movie[];
 
-  constructor(http: HttpClient, @Inject('API_URL') apiUrl: string) {
-    http.get<Movie[]>(apiUrl + 'movies').subscribe(result => {
-      this.movies = result;
-    }, error => console.error(error));
+  constructor(private moviesService: MoviesService) {
+   
   }
 
+  getMovies() {
+    this.moviesService.getMovies().subscribe(m => this.movies = m)
+  }
 
   ngOnInit() {
+    this.getMovies();
   }
 
 }
